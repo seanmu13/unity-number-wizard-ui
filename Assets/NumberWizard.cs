346268848b0c867d7e18a826acf77b7159b7b511
Sean Murray <seanmu13@gmail.com>
2015-04-07 11:37:26 -0400

    Lecture 60 - GIMP Image Editing 101

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAllowed = 6;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		NextGuess();
+	}
+	
+	public void GuessLower() {
+		max = guess - 1;
+		NextGuess();
+	}
+	
+	public void GuessHigher() {
+		min = guess + 1;
+		NextGuess();
+	}
+	
+	void NextGuess() {
+		guess = Random.Range (min,max + 1);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		
+		if(maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
